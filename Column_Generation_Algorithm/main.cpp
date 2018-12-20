19db363633bd23b21a0d3c1bfea6bf179d38358b
jingjinghuo <huo841022920@163.com>
2018-12-20 19:26:22 +0100

    Initial Commit

---
+++ Column_Generation_Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Column_Generation_Algorithm
+//
+//  Created by Jingjing Huo on 20.12.18.
+//  Copyright © 2018 Jingjing Huo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
